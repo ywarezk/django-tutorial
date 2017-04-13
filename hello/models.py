bd54875d9ac466df065b7a4be146cec93a3cedd0
Tom Chen <tomchen4@gmail.com>
2017-04-13 14:08:59 +0300

    After templates

--- hello/models.py
+++ hello/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Greeting(models.Model):
+    message = models.CharField(max_length=300)
+    user = models.CharField(max_length=100, blank=True)
--- hello/views.py
+++ hello/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from hello.models import Greeting
 
 def hello_world(request):
-    return render(request, 'hello/hello-world.html')
+    greetings = Greeting.objects.all()
+    return render(request, 'hello/hello-world.html', context={
+        'greeting': "what's up",
+        'my_func': lambda : 'hello world from function',
+        'my_array': [1,2,3, 'hello'],
+        'condition': True,
+        'footer_message': 'Gil & Tom',
+        'greetings': greetings
+    })
 
 
 
