0e1293a84a017a4d2de159a994f483f879d06ebf
Tom Chen <tomchen4@gmail.com>
2017-04-13 15:41:51 +0300

    After forms

---
+++ hello/forms.py
@@ -0,0 +1,7 @@
+from django.forms import forms, fields, models
+from hello.models import Greeting
+
+class GreetingForm(models.ModelForm):
+    class Meta:
+        model = Greeting
+        exclude = ()
--- hello/views.py
+++ hello/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
 from hello.models import Greeting
+from hello.forms import GreetingForm
 
 def hello_world(request):
+    if request.method == "POST":
+        my_form = GreetingForm(request.POST)
+        valid = my_form.is_valid()
+        if valid:
+            greeting = my_form.save()
     greetings = Greeting.objects.all()
+    my_form = GreetingForm()
     return render(request, 'hello/hello-world.html', context={
         'greeting': "what's up",
         'my_func': lambda : 'hello world from function',
         'my_array': [1,2,3, 'hello'],
         'condition': True,
         'footer_message': 'Gil & Tom',
-        'greetings': greetings
+        'greetings': greetings,
+        'greeting_form': my_form
     })
 
 
